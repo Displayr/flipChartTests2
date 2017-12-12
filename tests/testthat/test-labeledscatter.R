8f5722a0f587457fb4e680118a0f5a64c9d51167
chschan <chschan@gmail.com>
2017-12-12 14:16:31 +1100

    Move tests (scatter, labeledscatter, pie) from flipChartTests

---
+++ tests/testthat/test-labeledscatter.R
@@ -0,0 +1,37 @@
+context("Labeled scatter")
+
+set.seed(12358)
+dat <- matrix(rnorm(24), 12, 2,
+              dimnames=list(stringi::stri_rand_strings(12, 6), c("X", "Y")))
+zgroup <- rep(LETTERS[1:3], 4)
+logos <- sprintf("https://displayrcors.azureedge.net/images/%s_grey.svg",
+                 c("apple", "soup", "bread", "car", "chicken", "rocket",
+                   "sickperson", "thumbsup", "elephant", "tree", "weight", "tools"))
+
+test_that("Max labels", {
+    expect_warning(pp <- LabeledScatter(dat, scatter.max.labels = 5), "Some labels have been hidden")
+    expect_true(TestWidget(pp, "labeledscatter-maxlabels"))
+})
+
+test_that("Logos",  {
+    expect_error(pp <- LabeledScatter(dat, logos = logos), NA)
+    expect_true(TestWidget(pp, "labeledscatter-logos"))
+    expect_error(pp <- LabeledScatter(dat, logos = paste(logos, collapse=","), logo.size = 0.2), NA)
+    expect_true(TestWidget(pp, "labeledscatter-logos-resized"))
+    expect_error(pp <- LabeledScatter(dat, logos = c("Typo", logos[-1]), logo.size = 0.2), NA)
+    expect_true(TestWidget(pp, "labeledscatter-logos-typo"))
+})
+
+test_that("Trend lines", {
+    expect_error(pp <- LabeledScatter(dat, trend.line = TRUE), NA)
+    expect_true(TestWidget(pp, "labeledscatter-trend-single"))
+    expect_error(pp <- LabeledScatter(dat, scatter.colors = zgroup,
+                                      scatter.colors.as.categorical = T, trend.line = TRUE), NA)
+    expect_true(TestWidget(pp, "labeledscatter-trend-groups"))
+    expect_error(pp <- LabeledScatter(list(dat, dat+0.5, dat+1), trend.line = TRUE), NA)
+    expect_true(TestWidget(pp, "labeledscatter-trend"))
+    expect_warning(pp <- LabeledScatter(list(dat, dat+0.5, dat+1), trend.line = FALSE), "Tables have been automatically assigned names")
+    expect_true(TestWidget(pp, "labeledscatter-notrend"))
+    expect_error(pp <- LabeledScatter(list(dat, dat+rnorm(24)), trend.line = TRUE, logos = logos, logo.size = 0.2), NA)
+    expect_true(TestWidget(pp, "labeledscatter-trend-logos"))
+})
---
+++ tests/testthat/test-scatter.R
@@ -0,0 +1,53 @@
+context("Scatter plot")
+library("flipChartBasics")
+library("flipStandardCharts")
+
+# Set up dataframe containing different types of data types
+set.seed(1234)
+dat <- data.frame('Score' = rnorm(20),
+                  'Cost ($)' = abs(rnorm(20)), # check plotly is handling '$' properly
+                  'Age' = rpois(20, 40),
+                  'Class' = factor(sample(LETTERS[4:1], 20, replace = TRUE), levels = LETTERS[4:1]), # reverse order to check DS-1645
+                  'Sporadic' = c(1:5, NA, 6:10, NA, NA, 11:12, NA, NA, 13:15), # missing values
+                  'Date' = as.Date(sprintf("2017-01-%02d", 20:1)),
+                   check.names = FALSE, stringsAsFactors = FALSE)
+rownames(dat) <- letters[1:20]
+
+# Set up matrix to use the different variable types
+grid2 <- expand.grid(0:6, 0:6)
+tmp <- cbind(rbind(c(NA, 6), c(6, NA), grid2[2:49,]), grid2[c(1, 49:1),])
+
+columns.str <- sprintf("scatter.x.column = %.0f, scatter.y.column = %d,
+                        scatter.colors.column = %d, scatter.sizes.column = %d",
+                       tmp[,1], tmp[,2], tmp[,3], tmp[,4])
+names(columns.str) <- apply(tmp, 1, paste, collapse="")
+
+# These are only the options that can be used by both Labeled and (plotly) Scatterplots
+# Line of best fit is already tested in test-backgrounds.R
+opts <- c('default' = 'colors = ChartColors(5, "Blues")',
+         'categoricalcolor' = 'scatter.colors.as.categorical = TRUE, legend.font.color = "red"',
+         'numericalcolor' = 'scatter.colors.as.categorical = FALSE, colors = grey(1:4/5)',
+         'nolegend' = 'legend.show = FALSE, colors = "red"',
+         'markerbig' = 'series.marker.size = 20, grid.show = FALSE',
+         'thickxgrid' = 'x.grid.width = 10, global.font.color = "red", global.font.family = "Courier"')
+
+n <- length(opts)
+index <- 1
+for (func in c("Scatter", "LabeledScatter"))
+{
+    for (ii in 1:length(columns.str))
+    {
+        jj <- n - (index %% n)
+        filestem <- paste0(tolower(func), "-", names(columns.str)[ii], "-", names(opts)[jj])
+        test_that(filestem, {
+
+            cmd <- paste0("pp <- ", func, "(dat, ", columns.str[ii], ", ", opts[jj], ")")
+            expect_error(suppressWarnings(eval(parse(text = cmd))), NA)
+
+            #print(pp)
+            #readline(prompt=paste0(filestem, ": press [enter] to continue: "))
+            expect_true(TestWidget(pp, filestem))
+        })
+        index <- index + 1
+    }
+}
