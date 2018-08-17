066c218118c9dfa991687d9ae8bd948566242302
chschan <chschan@gmail.com>
2018-08-17 15:58:09 +1000

    Fix previous commit

--- tests/testthat/test-timeaxis.R
+++ tests/testthat/test-timeaxis.R
@@ -7,7 +7,7 @@ for (n in c(5, 10, 11, 15))
     xx <- cbind(A=1:n, B=(1:n)+1, C=(1:n)+2)
     for (step in c(1, 8, 25, 1e3, 1e4, 1e5, 1e6, 1e7))
     {
-        rownames(xx) <- as.character(time0 + (1:n * step))i
+        rownames(xx) <- as.character(time0 + (1:n * step))
         
         filestem <- sprintf("timeaxis-line-n%d-s%d", n, step)
         expect_error(pp <- Line(xx, data.label.show = TRUE), NA)
