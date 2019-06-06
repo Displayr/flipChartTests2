cb2216e971bdb9e7ee70babe1891fcd88a8adb63
chschan <chschan@gmail.com>
2019-06-06 12:01:09 +1000

    DS-2508: Add tests checking hover text

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -15,3 +15,15 @@ test_that("Vector of line widths",
     pp <- Line(named, line.thickness = "1,3,5")
     expect_true(TestWidget(pp, filestem))
 })
+
+one.row <- structure(c(8, 12, 67), .Dim = c(1L, 3L), 
+    .Dimnames = list("A", c("Detractors", "Passives", "Promotes")))
+test_that("Hover works with 1 row",
+{
+    # DS-2508: plotly seems to have problem converting single values to vectors
+    pp <- Bar(one.row, data.label.show = T, type = "Stacked")
+    expect_true(TestWidget(pp, "misc-hover-onerow-bar"))
+    
+    pp <- Column(one.row, data.label.show = T)
+    expect_true(TestWidget(pp, "misc-hover-onerow-column"))
+})
