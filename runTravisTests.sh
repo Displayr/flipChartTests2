e2af89867f51ea757cfe1ecd5ac455ea99707d8e
chschan <chschan@users.noreply.github.com>
2020-02-27 13:06:54 +1100

    DS-2817: Accept diffs (hist-bin)

--- runTravisTests.sh
+++ runTravisTests.sh
@@ -2,7 +2,7 @@
 
 if [ -d tests/testthat ]; then
     echo "tfile <- tempfile()" > test.R
-    echo "capture.output(res<-devtools::test(), file = tfile)" >> test.R
+    echo "tfile <- tempfile(tmpdir = '.', fileext = '.txt')" > test.R
     echo "out <- readLines(tfile)" >> test.R
     echo "cat(out, sep = '\n')" >> test.R
     echo "n.fail <- as.numeric(sub('Failed:[[:space:]]', '', out[grep('Failed:[[:space:]]', out)]))" >> test.R
