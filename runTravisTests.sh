1b6c2907e9336cce93d575c63959d0c7e9810c88
chschan <chschan@users.noreply.github.com>
2022-02-27 17:33:46 +1100

    Re-run tests

--- runTravisTests.sh
+++ runTravisTests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ -d tests/testthat ]; then
     echo "tfile <- tempfile(tmpdir = '.', fileext = '.txt')" > test.R
     echo "options('testthat.progress.max_fails' = Inf)" >> test.R
